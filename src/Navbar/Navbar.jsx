43d51f2e06c7ade16bca792863502c6000a36bbd
unknown <harshdeep4898@gmail.com>
2021-04-11 16:42:01 +0530

    fixed collapse navbar issue

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,29 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import  './Navbar.css'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import {Link} from 'react-router-dom'
-const navbar= (props)=>{
+
+
+
+const Navbard= (props)=>{
+    const [expanded, setExpanded] = useState(false);
     return(
+      
         <div>
-            <Navbar className='navbar' fixed="top"  expand="lg">
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar expanded={expanded} className='navbar' fixed="top"  expand="lg">
+                <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                <Link className='nav-links' to={{pathname:'/my-portfolio/'}}>About Me</Link>
-                <Link className='nav-links' to={{pathname:'/my-portfolio/myskills'}} >Technical Skills</Link>
-                <Link className='nav-links' to={{pathname:'/my-portfolio/myprojects'}}>My Projects</Link>    
+                <Link onClick={() => setExpanded(false)} className='nav-links' to={{pathname:'/my-portfolio/'}}>About Me</Link>
+                <Link onClick={() => setExpanded(false)} className='nav-links' to={{pathname:'/my-portfolio/myskills'}} >Technical Skills</Link>
+                <Link onClick={() => setExpanded(false)} className='nav-links' to={{pathname:'/my-portfolio/myprojects'}}>My Projects</Link>    
                 </Nav>
               </Navbar.Collapse>
             </Navbar>
-
-            {/* <div className='navbar'>
-            
-            <a href="https://www.surveymonkey.com/r/M9FS3Z5" target="_blank" className='nav-links'>Contact Me</a>
-            </div> */}
         </div>
     )
+    
 
 }
 
-export default navbar
+export default Navbard
